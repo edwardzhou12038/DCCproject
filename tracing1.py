95ab28700ceb67e9322e97af2341030203c954b9
edwardzhou12038 <35828619+edwardzhou12038@users.noreply.github.com>
2022-04-14 14:40:49 -0400

    Add files via upload
    interestingly small result

--- tracing1.py
+++ tracing1.py
@@ -2,6 +2,7 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
+    SimpleSpanProcessor,
     ConsoleSpanExporter,
 )
 from hwcounter import Timer, count, count_end
@@ -13,7 +14,7 @@ from time import perf_counter, perf_counter_ns, sleep
 
 
 provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
@@ -25,11 +26,13 @@ for i in range(1000):
     start = count()
 
     parent_span = tracer.start_as_current_span("foo")
-
-    parent_span.end()
+    span = trace.get_current_span()
+    
+    span.end()
     end = count_end()
     toc = time.perf_counter_ns()
     elapsed = end - start
     elapsed_time = "{:.4f}".format((toc - tic) / 1000000)
-    # print(f'elapsed cycles: {elapsed}')
+    #print(f'elapsed cycles: {elapsed}')
+    #print(f'elapsed time: {elapsed_time}')
     sys.stderr.write("\r%d , %s" %(elapsed,elapsed_time))
